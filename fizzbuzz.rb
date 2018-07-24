d0ec0b1fbdd236847f4a62380dd43453e738e212
elizabeth1018 <lizz_101@icloud.com>
2018-07-24 18:47:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 number= 15
 
-if number %3 == 0
-  puts "Fizz"
+if number %15 == 0
+  puts "Fizzbuzz"
 elsif number %5 == 0
   puts "Buzz"
-elsif number %15 == 0
-  puts "Fizzbuzz"
+elsif number %3 == 0
+  puts "Fizz"
   
 end 
